b341743677acc74002a8597bd05423fb23fc329d
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-05-17 16:25:57 +0700

    Melengkapi preferences design

--- lib/core/preferences/assets.dart
+++ lib/core/preferences/assets.dart
@@ -4,4 +4,8 @@ class MainAssets {
   static const path = 'assets/images';
 
   static const logo = '$path/logo.png';
+  static const empty = '$path/illustration_empty.png';
+  static const error = '$path/illustration_error.png';
+  static const printer = '$path/icon_printer.png';
+  static const success = '$path/icon_success.png';
 }
---
+++ lib/core/preferences/dimens.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Dimens {
+  Dimens._();
+
+  static width(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static height(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static const dp2 = 2.0;
+  static const dp4 = 4.0;
+  static const dp8 = 8.0;
+  static const dp10 = 10.0;
+  static const dp12 = 12.0;
+  static const dp14 = 14.0;
+  static const dp16 = 16.0;
+}
